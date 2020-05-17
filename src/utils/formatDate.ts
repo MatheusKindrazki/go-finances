cffec8e9726f5e960d9810eabddc4b81c7cdd97c
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-05-16 23:11:32 -0300

    enviando desafio para rocketseat

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,1 @@
+export default (date: Date) => new Intl.DateTimeFormat('en-GB').format(date)
---
+++ src/utils/formatValue.ts
@@ -0,0 +1,2 @@
+export default (value: number): string =>
+  Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value)
